8624b424b1bc6ccc3f41f6ea6bf6ef4ea781d87e
Lia Cui <yangyangjiaocui@gmail.com>
2023-04-17 09:48:15 -0700

    rocket movement coded

--- src/prefabs/Rocket.js
+++ src/prefabs/Rocket.js
@@ -6,4 +6,33 @@ class Rocket extends Phaser.GameObjects.Sprite {
         // add objext to existing scene
         scene.add.existing(this);
     }
+
+    update() {
+        // left/right movement
+        if(!this.isFiring){
+            if(keyLEFT.isDown && this.x >= borderUISize + this.width){
+                this.x -= this.moveSpeed;
+
+            } else if (keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
+                this.x += this.moveSpeed;
+
+            }
+
+        }
+
+        // fire button
+        if(Phaser.Input.Keyboard.JustDown(keyF)){
+            this.isFiring = true;
+        }
+        // if fired, move up
+        if(this.isFiring && this.y >= borderUISize * 3 + borderPadding) {
+            this.y -= this.moveSpeed;
+        }
+        // reset on miss
+        if(this.y <= borderUISize * 3 + borderPadding){
+            this.isFiring = false;
+            this.y = game.config.height - borderUISize - borderPadding;
+        }
+    }
 }
+
